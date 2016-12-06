0a5c74f49747312c882d3cfd52f766b47267db96
Justin Rempel <justinrempel@gmail.com>
2016-12-05 22:25:45 -0800

    Starting items are now data driven instead of hard-coded

--- forum/Sources/Inventory.php
+++ forum/Sources/Inventory.php
@@ -180,69 +180,53 @@ function generateStarterInventory()
 
 	$result = array();
 
-	// choose some item ids
-	$ids = array(
-		2,	// Gold Body
-		4, 	// Blue Body
-		3, 	// Pink Body
-		7, 	// Red Jersey
-		8,	// Green Jersey
-		10,	// Dirty Socks
-		12,	// Sweatshorts
-		9,	// Kawaii Hairdo
-		6, 	// Bikini Top
-	);
+	// get all item data for the items 
+	$itemRequest = $smcFunc['db_query']('', '
+	                SELECT *
+					FROM {db_prefix}items
+	                WHERE availability = {int:startingItem}
+	                OR availability = {int:startingItemLocked}',
+	                array(
+	                	'startingItem' => ItemAvailability::StartingItem,
+	                    'startingItemLocked' => ItemAvailability::StartingItemLocked,
+	                )
+	            ); 
 
-	$whereClause = '';
-
-	for($i = 0; $i < count($ids); $i++)
+	while ($row = mysql_fetch_assoc($itemRequest)) 
 	{
-		// build the item query
-	  	if($whereClause == '')
+		$item = array();
+
+		// load all the attributes for this item and store them back in the result array
+		$item['id'] = $row['id_item'];
+		$item['count'] = 1;
+		$item['is_equipped'] = false;
+
+		$item['item_type'] = $row['item_type'];
+		$item['name_eng'] = $row['name_eng'];
+		$item['img_url'] = $row['img_url'];
+		$item['icon_url'] = $row['icon_url'];
+		$item['equip_slot'] = $row['equip_slot'];
+		$item['can_delete'] = $row['can_delete'];
+		$item['is_locked'] = $row['availability'] == ItemAvailability::StartingItemLocked;
+
+		$result[$row['id_item']] = $item;
+	}
+
+	// equip a random body
+	$bodyTypes = array();
+	foreach ($result as $id => $item) 
+	{
+		if($item['equip_slot'] === (string) EquipSlot::BodyBase)
 		{
-			$whereClause = 'WHERE id_item = ' . $ids[$i];
-		}
-		else
-		{
-			$whereClause = $whereClause . ' OR id_item = ' . $ids[$i];
+			$bodyTypes[] = $id;
 		}
 	}
 
-	if($whereClause != '')
+	if (!empty($bodyTypes))
 	{
-		// get all item data for the items 
-		$itemRequest = $smcFunc['db_query']('', '
-		                SELECT *
-						FROM {db_prefix}items ' .
-		                $whereClause
-		            ); 
-
-		while ($row = mysql_fetch_assoc($itemRequest)) 
-		{
-			$item = array();
-
-			// load all the attributes for this item and store them back in the result array
-			$item['id'] = $row['id_item'];
-			$item['count'] = 1;
-			$item['is_equipped'] = false;
-
-			$item['item_type'] = $row['item_type'];
-			$item['name_eng'] = $row['name_eng'];
-			$item['img_url'] = $row['img_url'];
-			$item['icon_url'] = $row['icon_url'];
-			$item['equip_slot'] = $row['equip_slot'];
-			$item['can_delete'] = $row['can_delete'];
-
-			$result[$row['id_item']] = $item;
-		}
+		$bodyId = $bodyTypes[array_rand($bodyTypes)];
+		$result[$bodyId]['is_equipped'] = true;
 	}
-
-	// equip a body
-	$result[2]['is_equipped'] = true;
-
-	// "lock" some items
-	$result[9]['is_locked'] = true;
-	$result[6]['is_locked'] = true;
 
 	return $result;
 }
