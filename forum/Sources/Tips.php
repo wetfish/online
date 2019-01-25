5a955b67f074bcc32dcf8deb45b57299e4b0e1f0
Yam <2872995+Yam-s@users.noreply.github.com>
2019-01-25 03:09:28 +0000

    WIP Tip page template to match threads

--- forum/Sources/Tips.php
+++ forum/Sources/Tips.php
@@ -7,8 +7,6 @@ function Tips()
 {
 	global $context, $smcFunc, $user_info, $txt;
 
-	is_not_guest();
-
 	$context['page_title'] = $txt['tip_list_title'];
 	
 	loadPosts();
@@ -18,7 +16,7 @@ function Tips()
 
 function loadPosts()
 {
-	global $context, $smcFunc, $user_info;
+	global $memberContext, $context, $smcFunc, $user_info;
 	
 	$tippedPosts = array();
 	
@@ -28,27 +26,37 @@ function loadPosts()
 				SELECT id_message_tip, id_message, id_member, coins
 				FROM {db_prefix}message_tips
 				ORDER BY id_message_tip DESC
-				'); 
+				');
 	
 	// Get post associated with each tip
 	while($tip = $smcFunc['db_fetch_assoc']($tipsQuery))
 	{
 		$tippedPostsQuery = $smcFunc['db_query']('', '
-							SELECT id_msg, id_member, body, id_topic, poster_name
+							SELECT id_msg, id_member, body, id_topic, poster_name, poster_time, icon, subject
 							FROM {db_prefix}messages
 							WHERE id_msg = {int:id_msg}',
 							array(
 								'id_msg' => $tip['id_message']
 							));
+		$result = $smcFunc['db_fetch_assoc']($tippedPostsQuery);
 
-		// Store posts and tips in array 'post_id' = [post, tips[]]
+		// Load OP data into memberContext
+		loadMemberData(array($result['id_member']), false, 'minimal');
+		loadMemberContext($result['id_member']);
+
+		// Load Tipper data into memberContext
+		loadMemberData(array($tip['id_member']), false, 'minimal');
+		loadMemberContext($tip['id_member']);
+
 		$tippedPosts[$tip['id_message']] =
 		array(
-			'post' => $smcFunc['db_fetch_assoc']($tippedPostsQuery),
+			'poster' => $memberContext[$result['id_member']],
+			'tipper' => $memberContext[$tip['id_member']],
+			'post' => $result,
 			'tips' => array_merge_recursive( (array)$tippedPosts[$tip['id_message']]['tips'], array($tip))
 		);
+		$tippedPosts[$tip['id_message']]['post']['href'] = 'index.php?topic=' . $result['id_topic'] . '.msg' . $result['id_msg'] . '#msg' . $result['id_msg'];
 	}
-	
 	$context['recent_tipped_posts'] = $tippedPosts;
 }
 ?>
--- forum/Themes/default/Tips.template.php
+++ forum/Themes/default/Tips.template.php
@@ -12,7 +12,7 @@
 
 function template_main()
 {
-	global $context, $txt, $scripturl;
+	global $context, $txt, $scripturl, $settings;
 
 	// header
 	echo '<div class="cat_bar">
@@ -20,9 +20,9 @@ function template_main()
 		</div>';
 
 	// begin content
-	echo '<div class="windowbg2">
+	echo '<div class="forumposts">
 		<span class="topslice"><span></span></span>
-		<div class="content">';
+		<div class="body_message">';
 
 	foreach ($context['recent_tipped_posts'] as $tippedPost)
 	{
@@ -33,15 +33,63 @@ function template_main()
 			$sum += $tip['coins'];
 		}
 		
-		echo sprintf("Post #<a href=index.php?topic=%d#msg%d>%d</a> by %s tipped %d times. total: %d <br />", 
-		$tippedPost['post']['id_topic'],
-		$tippedPost['post']['id_msg'],
-		$tippedPost['post']['id_msg'],
-		$tippedPost['post']['poster_name'],
-		count($tippedPost['tips']),
-		$sum);	
+		echo '
+			<div class="body_message">
+				<div class="row">
+					<div class="poster col-md-2">';
+					// Load avatar
+					echo '
+							<script type="text/javascript">
+								$(document).ready(function() { 
+									loadAvatar(\'',json_encode(loadInventory($tippedPost['poster']['id'], true)),'\', "fishcanvas_',  $tippedPost['post']['id_msg'], '", "fish_avatar_img_',  $tippedPost['post']['id_msg'],'");
+								});
+							</script>';
+					// Display avatar
+					echo '
+							<canvas id="fishcanvas_',$tippedPost['post']['id_msg'],'" width="',FISH_WIDTH,'" height="',FISH_HEIGHT,'" style="display:none"></canvas>
+							<img id="fish_avatar_img_',$tippedPost['post']['id_msg'],'" alt="', sprintf($txt['fish_avatar_img_alt'], $tippedPost['poster']['name']),'">
+					</div>';
+		
+					echo '
+					<div class="col-md-10">
+						<div class="body_content">
+							<span class="arrow-left"></span>
+							<div class="postarea">
+								<div class="keyinfo">
+									<div class="messageicon">
+										<img src="' . $settings['images_url'] . '/post/' . $tippedPost['post']['icon'] . '.gif"/>
+									</div>
+									<h5 id="subject_', $tippedPost['post']['id'], '">
+										<a href="', $tippedPost['post']['href'], '" rel="nofollow">', $tippedPost['post']['subject'], '</a>
+									</h5>
+									<div class="smalltext">&#171; <strong>', $txt['on'], ':</strong> ', timeformat($tippedPost['post']['poster_time']), ' &#187;</div>
+									<div id="msg_', $message['id'], '_quick_mod"></div>
+								</div>';
+								
+								echo '
+								<div class="post">
+									<div class="inner" id="msg_' . $tippedPost['post']['id_msg'] . '">';
+									echo $tippedPost['post']['body'];
+								echo '
+									</div>
+								</div>';
+							
+								echo sprintf('<br />
+								<p class="post-tip-notice">Post #<a href=index.php?topic=%d#msg%d>%d</a> by %s tipped %d times. total: %d </p><br />', 
+								$tippedPost['post']['id_topic'],
+								$tippedPost['post']['id_msg'],
+								$tippedPost['post']['id_msg'],
+								$tippedPost['post']['poster_name'],
+								count($tippedPost['tips']),
+								$sum);
+				
+							echo '
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>';
 	}
-
 	// end content
 	echo'
 		</div>
